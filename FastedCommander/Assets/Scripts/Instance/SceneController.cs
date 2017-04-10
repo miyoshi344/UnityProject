1de20224ae042a74dd1ebc8d67642d0c2d767bf4
twinz <twinz344@gmail.com>
2017-04-10 18:28:42 +0900

    ファイル追加

---
+++ FastedCommander/Assets/Scripts/Instance/SceneController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * シーン遷移管理用コントローラー
+ */ 
+public class SceneController : MonoBehaviour {
+
+    public static SceneController instance;
+
+    private void Awake()
+    {
+        // SceneCntrollerインスタンスが存在したら
+        if(instance != null)
+        {
+            // 今回インスタンス化したSceneControllerを破棄
+            Destroy(this.gameObject);
+        }
+        else if(instance == null)
+        {
+            // このSceneControllerをインスタンスとする
+            instance = this;
+        }
+
+        // シーンを跨いでもSceneControllerインスタンスを破棄しない
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    static public void ChangeScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    // Use this for initialization
+    void Start () {
+        // タイトルシーン呼び出し
+        SceneManager.LoadScene("TitleScene");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ FastedCommander/Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // 画面遷移
+    public void TouchStart()
+    {
+        SceneController.ChangeScene("MainScene");
+    }
+}
