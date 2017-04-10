ed9474df88648da78d7779ecdf2bd12363370627
twinz <twinz344@gmail.com>
2017-04-10 19:12:49 +0900

    デバッグ機能の追加

---
+++ FastedCommander/Assets/Scripts/Instance/Debug.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Debug : MonoBehaviour
+{
+
+    public Text SceneTitleText;
+    public Text FpsText;
+
+    private int frameCount;
+    private float prevTime;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // EDBUGモードなら
+#if DEBUG
+
+        SceneTitleText.gameObject.SetActive(true);
+        FpsText.gameObject.SetActive(true);
+
+        // 初期化
+        frameCount = 0;
+        prevTime = 0.0f;
+
+        // シーンを跨いでもDebugインスタンスを破棄しない
+        DontDestroyOnLoad(this.gameObject);
+
+#else
+
+        SceneTitleText.gameObject.SetActive(false);
+        FpsText.gameObject.SetActive(false);
+#endif
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if DEBUG
+
+        ++frameCount;
+
+        float time = Time.realtimeSinceStartup - prevTime;
+
+        // フレームレートの更新
+        if (time >= 0.5f)
+        {
+            FpsText.text = Mathf.Floor(frameCount - time) + "fps";
+
+            frameCount = 0;
+            prevTime = Time.realtimeSinceStartup;
+        }
+
+        // タイトルの更新
+        SceneTitleText.text = SceneManager.GetActiveScene().name;
+#endif
+    }
+}
